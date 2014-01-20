76b7175af1c32a871d8dc8338946056046a9ebaf
Jose <jtoro2716@gmail.com>
2014-01-20 16:20:15 -0600

    Hyperion-ize

--- hyperion.mk
+++ hyperion.mk
@@ -1,28 +1,26 @@
-# Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/cdma.mk)
-
-# Release name
-PRODUCT_RELEASE_NAME := i605
-
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
-
 # Inherit device configuration
 $(call inherit-product, device/samsung/i605/full_i605.mk)
 
+# Specify phone tech before including full_phone
+$(call inherit-product, vendor/hyperion/config/cdma.mk)
+
+# Inherit some common hyperion stuff.
+$(call inherit-product, vendor/hyperion/config/common_full_phone.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := i605
+
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := i605
-PRODUCT_NAME := cm_i605
+PRODUCT_NAME := hyperion_i605
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SCH-I605
 PRODUCT_MANUFACTURER := samsung
-
 PRODUCT_GMS_CLIENTID_BASE := android-verizon
 
 # Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=t0ltevzw TARGET_DEVICE=t0ltevzw BUILD_FINGERPRINT="Verizon/t0ltevzw/t0ltevzw:4.1.2/JZO54K/I605VRAMC3:user/release-keys" PRIVATE_BUILD_DESC="t0ltevzw-user 4.1.2 JZO54K I605VRAMC3 release-keys"
-
